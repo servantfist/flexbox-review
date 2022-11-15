8dccc67d2caafff334e8640832cea40209c3e186
Ken Larson <servantfist@users.noreply.github.com>
2022-11-14 20:26:22 -0700

    Updated to Express

--- app.js
+++ app.js
@@ -2,19 +2,28 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const port = process.env.PORT || 3500;
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
 
 var app = express();
+app.use(express.static(__dirname + '/public'));
+
+const router = express.Router();
+
+router.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, '/index.html'));
+    //__dirname : It will resolve to your project folder.
+});
+
+app.use('/', router);
+app.listen(port);
+console.log('Server started at http://localhost:' + port);
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 module.exports = app;
